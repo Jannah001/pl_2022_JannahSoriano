c8952970104d1e5d0df890a172f6617b25c8c514
unknown <you@example.com>
2022-11-21 20:32:52 +0800

    Lab 1

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Palindrome {
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        String str = scanner.nextLine();
+
+        int j, s;
+        j = 0;
+        s = str.length() - 1;
+
+        while (j < s) {
+            while (str.charAt(j) == ' '){
+                j++;
+            }
+            if(str.equalsIgnoreCase(String.valueOf(j)) == str.equalsIgnoreCase(String.valueOf(s)) ){
+                j++;
+                s--;
+            }
+            else {
+                break;
+            }
+        }
+        //checking whether palindrome or not
+        if (j <= s) {
+            System.out.println(str + " is a palindrome ");
+        } else {
+            System.out.println(str + " is NOT a palindrome");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
